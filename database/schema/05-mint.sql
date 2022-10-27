e137b0d9d9a627760db4a1b1b328da3f6b986914
apocalypse <nibir.pal@stlabs.tech>
2022-10-27 15:42:45 +0530

    feat: modifying bdjuno to be compatible with sge-v0.0.1

---
+++ database/schema/05-mint.sql
@@ -0,0 +1,30 @@
+/* ---- PARAMS ---- */
+
+CREATE TABLE mint_params
+(
+    one_row_id BOOLEAN NOT NULL DEFAULT TRUE PRIMARY KEY,
+    params     JSONB   NOT NULL,
+    height     BIGINT  NOT NULL,
+    CHECK (one_row_id)
+);
+
+/* ---- INFLATION ---- */
+
+CREATE TABLE inflation
+(
+    one_row_id bool PRIMARY KEY DEFAULT TRUE,
+    value      DECIMAL NOT NULL,
+    height     BIGINT  NOT NULL,
+    CONSTRAINT one_row_uni CHECK (one_row_id)
+);
+CREATE INDEX inflation_height_index ON inflation (height);
+
+/* ---- PHASE-STEP ----*/
+
+CREATE TABLE phase_step
+(
+    one_row_id bool PRIMARY KEY DEFAULT TRUE,
+    value      INT NOT NULL,
+    height     BIGINT  NOT NULL,
+    CONSTRAINT one_row_uni CHECK (one_row_id)
+)
